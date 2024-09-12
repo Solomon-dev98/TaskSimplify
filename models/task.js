497c2622f50322c25cc75624157dfae5762e5a1b
Solomon-dev98 <asapnino01@gmail.com>
2024-09-12 19:03:21 +0100

    add task model

---
+++ models/task.js
@@ -0,0 +1,16 @@
+import pgPromise from 'pg-promise';
+
+// Initialize database connection
+const pgp = pgPromise();
+const db = pgp('postgres://username:password@localhost:5432/tasksimplify');
+
+// Task model with different methods
+
+const Task = {
+    getAll: () => db.any('SELECT * FROM tasks'),
+    create: (taskData) => db.none('INSERT INTO tasks(title, description) VALUES($1, $2)', [taskData.title, taskData.description]),
+    update: (id, taskData) => db.none('UPDATE tasks SET title=$1, description=$2 WHERE id=$3', [taskData.title, taskData.description, id]),
+    delete: (id) => db.none('DELETE FROM tasks WHERE id=$1', [id]),
+};
+
+export default Task;
