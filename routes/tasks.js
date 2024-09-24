e13cf158117c076157c2786518bda2de01b524ed
Solomon-dev98 <asapnino01@gmail.com>
2024-09-24 11:12:08 +0100

    add tasks routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,18 +1,18 @@
 import express from 'express';
-import * as taskController from '../controllers/controller.js'; //import all exported functions
-
+import * as taskController from '../controllers/taskController.js'; //import all exported functions
+import { verifyToken } from '../middlewares/authMiddleware.js' // import middleware
 const router = express.Router();
 
 // routes to get all tasks
-router.get('/', taskController.getAllTasks);
+router.get('/', verifyToken, taskController.getAllTasks);
 
 // route to create a new task
-router.post('/', taskController.createTask);
+router.post('/', verifyToken, taskController.createTask);
 
 // route to update a task
-router.put('/:id', taskController.updateTask);
+router.put('/:id', verifyToken, taskController.updateTask);
 
 // route to delete task
-router.delete('/:id', taskController.deleteTask);
+router.delete('/:id', verifyToken, taskController.deleteTask);
 
 export default router;
