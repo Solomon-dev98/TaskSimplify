ccbb85eb0ce86852580967a1600c1124becb1a1f
Solomon-dev98 <asapnino01@gmail.com>
2024-10-24 11:33:12 +0100

    feat(json): Use json for response message

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,20 +1,29 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // Middleware to verify JWT token
 
 export const verifyToken = (req, res, next) => {
     const token = req.headers['authorization'];
+    console.log('Authorization header:', token);
 
-    if (!token) {
-        return res.status(403).send('Access denied. No token provided');
+    if (!token || !token.startsWith('Bearer ')) {
+        return res.status(403).json({ success: false, message: 'Access denied. No token provided'});
     }
 
     try {
         // verify token
-        const decoded = jwt.verify(token.split(' ')[1], 'your_jwt_secret');
+        const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
         req.user = decoded; // store the decoded user information in the request object
         next();
     } catch (error) {
-        return res.status(401).send('Invalid token.');
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ success: false, message: 'Token has expired.' });
+        } else if (error.name === 'JsonWebTokenError') {
+            return res.status(401).json({ success: false, message: 'Invalid token.' });
+        }
+        return res.status(500).json({ success: false, message: 'Internal server error.' });
     }
 };
