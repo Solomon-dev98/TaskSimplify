e2867ac56a0bbd8550aacb0b36e345c7c62d564b
Solomon-dev98 <asapnino01@gmail.com>
2024-10-24 11:34:40 +0100

    feat(due_date): Add due_date for tasks

--- models/task.js
+++ models/task.js
@@ -4,8 +4,8 @@ import db from '../config/db.js';
 
 const Task = {
     getAll: () => db.any('SELECT * FROM tasks'),
-    create: (taskData) => db.none('INSERT INTO tasks(title, description) VALUES($1, $2)', [taskData.title, taskData.description]),
-    update: (id, taskData) => db.none('UPDATE tasks SET title=$1, description=$2 WHERE id=$3', [taskData.title, taskData.description, id]),
+    create: (taskData) => db.none('INSERT INTO tasks(title, description, due_date) VALUES($1, $2, $3)', [taskData.title, taskData.description, taskData.due_date]),
+    update: (id, taskData) => db.none('UPDATE tasks SET title=$1, description=$2, due_date=$3 WHERE id=$4', [taskData.title, taskData.description, taskData.due_date, id]),
     delete: (id) => db.none('DELETE FROM tasks WHERE id=$1', [id]),
 };
 
